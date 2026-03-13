d01e80ed6d35c169072df33a2c33e89edb8c6103
Beyza Özpınar <beyzamunireozpinar@gmail.com>
2026-03-13 13:14:07 +0300

    feat: added controller for cancelling api calls, handled selectMovie and added userRating.

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,16 +1,8 @@
 import { Children, useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const getAverage = (array) =>
   array.reduce((sum, value) => sum + value / array.length, 0);
-
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjQwYTdkYWUxYzJjYmI2MDQ4ZjJhYjU2NzE5MWQ0OCIsIm5iZiI6MTYyNjYwNDkyOC4zNjUsInN1YiI6IjYwZjQwNTgwZjkwYjE5MDA3NDZhM2FjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n1wlH5iw_r3Sd0p2jj8_tUot9Ex_ucCvcbfz6UQ_3sQ",
-  },
-};
 
 export default function App() {
   const [query, setQuery] = useState("last");
@@ -27,15 +19,36 @@ export default function App() {
     setSelectedMovie(null);
   }
 
+  function handleAddSelectedList(movie) {
+    setSelectedMovies((selectedMovies) => [...selectedMovies, movie]);
+    handleUnselectMovie();
+  }
+  function handleRemoveMovieFromList(id) {
+    setSelectedMovies((selectedMovies) =>
+      selectedMovies.filter((m) => m.id !== id),
+    );
+  }
+
   useEffect(() => {
     //first render (mount)
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     async function getMovies() {
       try {
         setLoading(true);
         setError("");
         const res = await fetch(
           `https://api.themoviedb.org/3/search/movie?query=${query}`,
-          options,
+          {
+            signal: signal,
+            method: "GET",
+            headers: {
+              accept: "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjQwYTdkYWUxYzJjYmI2MDQ4ZjJhYjU2NzE5MWQ0OCIsIm5iZiI6MTYyNjYwNDkyOC4zNjUsInN1YiI6IjYwZjQwNTgwZjkwYjE5MDA3NDZhM2FjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n1wlH5iw_r3Sd0p2jj8_tUot9Ex_ucCvcbfz6UQ_3sQ",
+            },
+          },
         );
         if (!res.ok) {
           throw new Error("Hata oluştu");
@@ -46,7 +59,8 @@ export default function App() {
 
         setMovies(data.results);
       } catch (err) {
-        setError(err.message);
+        if (err.name === "AbortError") setError("Aborted");
+        else setError(err.message);
       }
       setLoading(false);
     }
@@ -56,9 +70,10 @@ export default function App() {
       return;
     }
     getMovies();
+    return () => {
+      controller.abort();
+    };
   }, [query]);
-
-  console.log(selectedMovie);
 
   return (
     <>
@@ -83,16 +98,22 @@ export default function App() {
         </div>
         <div className="col-md-3">
           <ListContainer>
-            <>
-              <MyListSummary selectedMovies={selectedMovies} />
-              <MyMovieList selectedMovies={selectedMovies} />
-              {selectedMovie && (
-                <MovieDetails
-                  selectedMovie={selectedMovie}
-                  onUnselectMovie={handleUnselectMovie}
+            {selectedMovie ? (
+              <MovieDetails
+                selectedMovie={selectedMovie}
+                onUnselectMovie={handleUnselectMovie}
+                onAddMovieToList={handleAddSelectedList}
+                selectedMovies={selectedMovies}
+              />
+            ) : (
+              <>
+                <MyListSummary selectedMovies={selectedMovies} />
+                <MyMovieList
+                  selectedMovies={selectedMovies}
+                  onRemoveMovie={handleRemoveMovieFromList}
                 />
-              )}
-            </>
+              </>
+            )}
           </ListContainer>
         </div>
       </Main>
@@ -128,6 +149,7 @@ function Logo() {
     </div>
   );
 }
+
 function Search({ query, setQuery }) {
   return (
     <div className="col-4">
@@ -196,17 +218,123 @@ function formatDate(dateStr) {
   if (!dateStr) return "Tarih Yok";
   return dateStr.split("-").reverse().join(" ");
 }
-function MovieDetails({ selectedMovie, onUnselectMovie }) {
+
+function MovieDetails({
+  selectedMovie,
+  onUnselectMovie,
+  onAddMovieToList,
+  selectedMovies,
+}) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [movie, setMovie] = useState({});
+  const [userRating, setUserRating] = useState(null);
+
+  const isAddedToList = selectedMovies.map((m) => m.id).includes(selectedMovie);
+  const selectedMovieUserRating = selectedMovies.find(
+    (m) => m.id === selectedMovie,
+  )?.userRating;
+
+  function handleAddtoMovieList() {
+    const newMovie = { ...movie, userRating };
+    onAddMovieToList(newMovie);
+  }
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${selectedMovie}`,
+          options,
+        );
+        if (!res.ok) {
+          throw new Error("Hata oluştu");
+        }
+
+        const data = await res.json();
+        if (data.total_results === 0) throw new Error("Sonuç bulunamadı");
+        setMovie(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+      }
+    }
+    getMovieDetails();
+  }, [selectedMovie]);
+
   return (
-    <div className="alert alert-primary d-flex justify-content-between">
-      <p>{selectedMovie}</p>
-      <p
-        className="text text-danger"
+    <div className="card border p-2 mb-3">
+      <div
+        className="text text-danger align-self-end"
         style={{ cursor: "pointer" }}
         onClick={onUnselectMovie}
       >
         x
-      </p>
+      </div>
+      <button
+        className={`btn ${isAddedToList ? "btn-success" : "btn-primary"} mb-2`}
+        onClick={handleAddtoMovieList}
+        disabled={isAddedToList}
+      >
+        {isAddedToList ? "Listede" : "Listeye Ekle"}
+      </button>
+      {!isAddedToList && (
+        <div className="my-4">
+          <StarRating
+            size={20}
+            color="green"
+            maxRating={10}
+            onRating={setUserRating}
+          />
+        </div>
+      )}
+      {loading && <Loading />}
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-4">
+            <img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+              }
+              alt={movie.title}
+              className="card-img-top img-fluid rounded"
+            />
+          </div>
+          <div className="col-8">
+            <h6>{movie.title}</h6>
+            <i className="bi bi-calendar2-date me-1"></i>
+            <span>{formatDate(movie.release_date)}</span>
+            {movie.vote_average && (
+              <p>
+                <i className="bi bi-star-fill text-warning me-2"></i>
+                {movie.vote_average?.toFixed(1)}
+              </p>
+            )}
+            <p>
+              <i className="bi bi-hourglass-split text-warning me-2"></i>
+              {movie.runtime} dk
+            </p>
+            {selectedMovieUserRating && (
+              <p>
+                Değerlendirme :<i className="bi bi-stars text-success me-2"></i>
+                {selectedMovieUserRating}
+              </p>
+            )}
+          </div>
+          <div className="col-12 border-top p-3">
+            <p>{movie.overview}</p>
+            {movie.genres?.map((genre) => (
+              <span key={genre.id} className="badge text-bg-primary me-1">
+                {genre.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
+      {error && <ErrorMessage message={error} />}
     </div>
   );
 }
@@ -265,8 +393,9 @@ function Movie({ movie, onSelectMovie, selectedMovie }) {
 // }
 
 function MyListSummary({ selectedMovies }) {
-  const avarageRating = getAverage(selectedMovies.map((m) => m.rating));
-  const avarageDuration = getAverage(selectedMovies.map((m) => m.duration));
+  const avarageRating = getAverage(selectedMovies.map((m) => m.vote_average));
+  const avarageUserRating = getAverage(selectedMovies.map((m) => m.userRating));
+  const avarageDuration = getAverage(selectedMovies.map((m) => m.runtime));
   return (
     <div className="card mb-2">
       <div className="card-body">
@@ -277,8 +406,12 @@ function MyListSummary({ selectedMovies }) {
             {avarageRating.toFixed(2)}
           </p>
           <p>
-            <i className="bi bi-star-fill text-warning me-2"></i>
-            {avarageDuration} dk
+            <i className="bi bi-stars text-warning me-2"></i>
+            {avarageUserRating.toFixed(2)}
+          </p>
+          <p>
+            <i className="bi bi-hourglass-split text-warning me-2"></i>
+            {avarageDuration.toFixed()} dk
           </p>
         </div>
       </div>
@@ -286,35 +419,50 @@ function MyListSummary({ selectedMovies }) {
   );
 }
 
-function MyMovieList({ selectedMovies }) {
+function MyMovieList({ selectedMovies, onRemoveMovie }) {
   return selectedMovies.map((movie) => (
-    <MyListMovieCard movie={movie} key={movie.Id} />
+    <MyListMovieCard
+      movie={movie}
+      key={movie.id}
+      onRemoveMovie={onRemoveMovie}
+    />
   ));
 }
-function MyListMovieCard({ movie }) {
+
+function MyListMovieCard({ movie, onRemoveMovie }) {
   return (
     <div className="card mb-2" key={movie.Id}>
       <div className="row">
         <div className="col-4">
           <img
-            src={movie.Poster}
-            alt={movie.Title}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+            }
+            alt={movie.title}
             className="img-fluid rounded-start"
           />
         </div>
         <div className="col-8">
           <div className="card-body">
-            <h6 className="card-title">{movie.Title}</h6>
+            <h6 className="card-title">{movie.title}</h6>
             <div className="d-flex justify-content-between">
               <p>
                 <i className="bi bi-hourglass-split text-warning me-1"></i>
-                <span>{movie.duration} dk</span>
+                <span>{movie.runtime} dk</span>
               </p>
               <p>
                 <i className="bi bi-star-fill text-warning me-1"></i>
-                <span>{movie.rating}</span>
+                <span>{movie.vote_average?.toFixed(1)}</span>
               </p>
             </div>
+            <button
+              className="btn btn-sm btn-danger"
+              onClick={() => onRemoveMovie(movie.id)}
+            >
+              Filmi Sil
+            </button>
           </div>
         </div>
       </div>
